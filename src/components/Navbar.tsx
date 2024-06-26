5a20a9ad1d613502eebf2ec1cfdc63ac7bf8932b
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-06-26 03:36:39 +0200

    implement scroll detection for active navbar item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,50 @@
 import '@/styles/navbar.css';
+import { useEffect } from 'react';
 
 export default function Navbar() {
+  useEffect(() => {
+    const setActiveClass = () => {
+      const projectSection = document.querySelector('#projects');
+      if (projectSection instanceof HTMLElement) {
+        const position = projectSection.getBoundingClientRect().top;
+        if (position < 500) {
+          document.querySelector('#nav-about')?.classList.remove('active');
+          document.querySelector('#nav-projects')?.classList.add('active');
+        } else {
+          document.querySelector('#nav-about')?.classList.add('active');
+          document.querySelector('#nav-projects')?.classList.remove('active');
+        }
+      }
+    };
+    window.addEventListener('scroll', setActiveClass);
+    return () => window.removeEventListener('scroll', setActiveClass);
+  }, []);
   return (
     <nav className='navbar'>
       <div className='center-navbar'>
         <div className='brand'>
-          <a href='https://flowcv.me/gunnar-miklis' target='_blank' rel='noreferrer' className='link'>
+          <a
+            href='https://flowcv.me/gunnar-miklis'
+            target='_blank'
+            rel='noreferrer'
+            className='link'
+          >
             Gunnar Miklis
           </a>
-          <a href='https://github.com/gunnar-miklis' target='_blank' rel='noreferrer' className='link'>
+          <a
+            href='https://github.com/gunnar-miklis'
+            target='_blank'
+            rel='noreferrer'
+            className='link'
+          >
             JavaScript Web Developer
           </a>
         </div>
         <div className='navigation'>
-          <a href='#about' className='link'>
+          <a id='nav-about' href='#about' className='link active'>
             About
           </a>
-          <a href='#projects' className='link'>
+          <a id='nav-projects' href='#projects' className='link'>
             Projects
           </a>
         </div>
