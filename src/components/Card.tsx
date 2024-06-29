edd61aa6f8b6b2239f2a928d17b142e63d839422
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-06-30 01:31:47 +0200

    implement animated expanding card-image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,16 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LaunchRoundedIcon from '@mui/icons-material/LaunchRounded';
 import ButtonWithIcon from './ButtonWithIcon';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CloseFullIcon from '@mui/icons-material/CloseFullscreen';
+import OpenFullIcon from '@mui/icons-material/OpenInFull';
 import Chip from './Chip';
 import '@/styles/card.css';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+
+const initalImageHeight = 400;
+const initalContentHeight = 1000;
 
 export interface CardProps {
   children: ReactNode;
@@ -30,17 +37,86 @@ export default function Card({
   sourceCode,
   footnote,
 }: CardProps) {
+  const [isImageExpanded, setIsImageExpanded] = useState<boolean>(false);
+  const [imageHeight, setImageHeight] = useState<number>(initalImageHeight);
+  const [contentHeight, setContentHeight] = useState<number>(initalContentHeight);
+
+  function handleExpand() {
+    // FIXME: right now this only works on phone + tablet
+    if (!isImageExpanded) {
+      const animationImage = setInterval(() => {
+        setImageHeight((prevHeight) => {
+          if (prevHeight < initalContentHeight) {
+            return prevHeight + 10;
+          } else {
+            clearInterval(animationImage);
+            return prevHeight;
+          }
+        });
+      }, 9);
+
+      const animationContent = setInterval(() => {
+        setContentHeight((prevHeight) => {
+          if (prevHeight > 110) {
+            return prevHeight - 10;
+          } else {
+            clearInterval(animationContent);
+            return prevHeight;
+          }
+        });
+      }, 5);
+
+      setIsImageExpanded(true);
+    } else {
+      const animationImage = setInterval(() => {
+        setImageHeight((prevHeight) => {
+          if (prevHeight > initalImageHeight) {
+            return prevHeight - 10;
+          } else {
+            clearInterval(animationImage);
+            return prevHeight;
+          }
+        });
+      }, 9);
+
+      const animationContent = setInterval(() => {
+        setContentHeight((prevHeight) => {
+          if (prevHeight < initalContentHeight) {
+            return prevHeight + 10;
+          } else {
+            clearInterval(animationContent);
+            return prevHeight;
+          }
+        });
+      }, 5);
+
+      setIsImageExpanded(false);
+    }
+  }
+
   return (
     <article className={`window card ${className}`}>
       {/* TODO: modal, image preview */}
-      <div className='card-image'>
+      <div className='card-image' style={{ maxHeight: imageHeight }} onClick={() => handleExpand()}>
+        {!isImageExpanded ? (
+          <span id='expand-more'>
+            <OpenFullIcon />
+            {/* <ExpandMoreIcon fontSize='large' /> */}
+          </span>
+        ) : (
+          <span id='expand-less'>
+            <CloseFullIcon />
+            {/* <ExpandLessIcon fontSize='large' /> */}
+          </span>
+        )}
+
         {/* images */}
         {imageSources.map((source, i) => (
           <img key={i} src={source} />
         ))}
       </div>
 
-      <div className='card-content paper-spacing-md gap-md'>
+      <div className='card-content paper-spacing-md gap-md' style={{ maxHeight: contentHeight }}>
         {/* header */}
         <div className='card-inner'>
           <div className='card-header'>
