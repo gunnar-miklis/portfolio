6cc235a8c7849a6a07b38f1d342a36f8419ac9dd
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-07-02 16:29:26 +0200

    YAGNI

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import '@/styles/chip.css';
 
 export default function Chip({ children }: { children: ReactNode }) {
   return <span className='button chip annotation'>{children}</span>;
