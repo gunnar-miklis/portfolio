e8b346f6f86c73013a928662cde89829c5b42f92
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-07-10 00:03:27 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -9,57 +9,19 @@ export default function App() {
     <>
       <Navbar />
 
-      <section id='about'>
-        <Header />
-        <Terminal />
-      </section>
+      <main>
+        <section id='about'>
+          <Header />
+          <Terminal />
+        </section>
 
-      <section id='projects'>
-        <h1 id='projects-title' style={{ marginBottom: '1rem' }}>
-          Projects
-        </h1>
-        <HorizontalScrollGallery projects={projects} />
-      </section>
-
-      {/* TESTING: start styleguide */}
-      {/* <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div className='window paper'>
-        <div
-          className='paper-spacing-lg gap-md'
-          style={{ display: 'flex', flexDirection: 'column' }}
-        >
-          <div>
-            <h1>First Title</h1>
-            <h2>Subtitle</h2>
-            <h3>Heading</h3>
-            <p>paragraph text</p>
-            <p className='annotation'>annotation</p>
-            <span>
-              <a className='link'>Link</a>
-            </span>
-          </div>
-
-          <div className='paper paper-spacing-md' style={{ flexFlow: 'row wrap' }}>
-            <Chip>Chip</Chip>
-            <Chip>Chip</Chip>
-            <Chip>Chip</Chip>
-            <Chip>Chip</Chip>
-            <Chip>Chip</Chip>
-            <Chip>Chip</Chip>
-          </div>
-
-          <div className='paper paper-spacing-md' style={{ flexFlow: 'row wrap' }}>
-            <input className='input' placeholder='placeholder' />
-            <button className='button'>Button</button>
-          </div>
-        </div>
-      </div> */}
-      {/* TESTING: end styleguide */}
+        <section id='projects'>
+          <h1 id='projects-title' style={{ marginBottom: '1rem' }}>
+            Projects
+          </h1>
+          <HorizontalScrollGallery projects={projects} />
+        </section>
+      </main>
     </>
   );
 }
