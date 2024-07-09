c1a8a910dfc7e07a0ffbc1981fc6274e7d03c997
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-07-10 00:11:05 +0200

    abstract

---
+++ src/components/ExternalLink.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+interface ExternalLinkProps {
+  href: string;
+  title: string;
+  children?: ReactNode;
+}
+
+export default function ExternalLink({ href, title, children }: ExternalLinkProps) {
+  return (
+    <a
+      className='link'
+      href={href}
+      target='_blank'
+      rel='noreferrer'
+      title={`Visit ${title} (external website opens in new tab)`}
+      aria-label={`Visit ${title} (external website opens in new tab)`}
+    >
+      {children || title}
+    </a>
+  );
+}
