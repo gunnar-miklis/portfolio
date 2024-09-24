15329ee558a3312a8b8eb56921fa27953de91390
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-09-24 17:21:07 +0200

    add tests for getHeaderVersionFrom() function and for selectedVersion variable

---
+++ src/data/__tests__/header.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, expectTypeOf, it } from 'vitest';
+import { headers, selectedVersion, getHeaderVersionFrom } from '@data/header';
+
+describe('selectedVersion const variable', () => {
+  it('should be a version number that also exists in headers array', () => {
+    expect(selectedVersion).toBeGreaterThanOrEqual(1);
+    expect(selectedVersion).toBeLessThanOrEqual(headers.length);
+  });
+});
+
+describe('getHeaderVersionFrom()', () => {
+  const testHeaders = [
+    { version: 1, hook: 'Hook 1', context: 'lorem ipsum', offer: 'lorem ipsum' },
+    { version: 2, hook: 'Hook 2', context: 'lorem ipsum', offer: 'lorem ipsum' },
+    { version: 3, hook: 'Hook 3', context: 'lorem ipsum', offer: 'lorem ipsum' },
+  ];
+
+  it('should return a single header object out of the given array', () => {
+    const result = getHeaderVersionFrom(testHeaders, 1);
+
+    expectTypeOf(result).not.toBeArray();
+    expectTypeOf(result).toBeObject();
+    expect(result).toEqual(testHeaders[0]);
+  });
+
+  it('should return the selected header when it matches the given version', () => {
+    const testVersion = 1;
+    expect(getHeaderVersionFrom(testHeaders, testVersion).hook).toBe('Hook 1');
+  });
+
+  it('should return the latest header when the given version does not match', () => {
+    const testVersion = 99;
+
+    // @ts-expect-error - versions are dynamically inferred from the headers array. allowed versions are: 1 | 2 | 3
+    expect(getHeaderVersionFrom(testHeaders, testVersion).hook).toBe('Hook 3');
+  });
+});
