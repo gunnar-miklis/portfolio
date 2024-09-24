b0813842eaf222c8c6450362f9826d1bc1b26673
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-09-24 17:18:05 +0200

    adjust and optimize tests

--- src/data/__tests__/projects.test.ts
+++ src/data/__tests__/projects.test.ts
@@ -1,20 +1,25 @@
-import { describe, expect, it } from 'vitest';
-import { projects } from '@data/projects';
+import { describe, expect, expectTypeOf, it } from 'vitest';
+import { type ProjectCategories, projects } from '@data/projects';
 
 describe('the projects data array', () => {
-  it('should be not empty', () => {
-    expect(projects.length).toBeTruthy();
-  });
-  it('should have unique ids', () => {
-    const projectIds = projects.map(({ id }) => id);
-    const duplicates = projectIds.filter((id, idx) => projectIds.indexOf(id) !== idx);
+  const projectsLength = projects.length;
+  const projectIds = projects.map(({ id }) => id);
 
+  it('should be not empty', () => {
+    expect(projectsLength).toBeTruthy();
+  });
+
+  it('should have unique ids', () => {
+    const duplicates = projectIds.filter((id, idx) => projectIds.indexOf(id) !== idx);
     expect(duplicates).toHaveLength(0);
   });
+
   it('should have ids in a specific format: "240801" (YY/MM/ProjectNr)', () => {
-    const projectIds = projects.map(({ id }) => id);
+    const validYears = ['23', '24'];
+    const validMonth = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+
     for (const id of projectIds) {
-      // skip 0-9, those are "special" projects, don't include in the sorting logic.
+      // skip 0-9, because those are "special" projects, don't include in the sorting logic.
       if (id < 10) continue;
 
       // check that the id is a number with 6 digits. And the year is bigger than 2023.
@@ -24,25 +29,16 @@ describe('the projects data array', () => {
       const month = id.toString().slice(2, 4);
       const projectNumber = Number(id.toString().slice(4, 8));
 
-      const validYears = ['23', '24'];
-      const validMonth = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-
       expect(validYears.includes(year)).toBe(true);
       expect(validMonth.includes(month)).toBe(true);
       expect(projectNumber).toBeGreaterThan(0);
       expect(projectNumber).toBeLessThanOrEqual(99);
     }
   });
-  it('should have valid categories', () => {
-    const validCategories = [
-      'Semi-Professional',
-      'Educational Project',
-      'Practice.Learn.Improve.',
-      undefined,
-    ];
 
+  it('should have valid categories', () => {
     for (const { category } of projects) {
-      expect(validCategories.includes(category)).toBe(true);
+      expectTypeOf(category).toEqualTypeOf<ProjectCategories | undefined>();
     }
   });
 });
